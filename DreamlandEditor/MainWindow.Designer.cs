2f84d12118338c4463b62342a7620e590c6f2a37
Benjámin Bíró <dsgct9@INF.ELTE.HU>
2022-06-26 16:28:08 +0200

    Built initial UI design

--- DreamlandEditor/MainWindow.Designer.cs
+++ DreamlandEditor/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace MonoGame.Forms.DX {
-    partial class Form1 {
+    partial class MainWindow {
         /// <summary>
         /// Erforderliche Designervariable.
         /// </summary>
@@ -26,8 +26,10 @@ namespace MonoGame.Forms.DX {
         private void InitializeComponent() {
             this.PanelItemExplorer = new System.Windows.Forms.FlowLayoutPanel();
             this.PanelFileExplorer = new System.Windows.Forms.Panel();
-            this.MonoRenderWindow = new MonoGame.Forms.DX.Controls.SampleControl();
             this.PanelNavbar = new System.Windows.Forms.Panel();
+            this.ButtonFiles = new System.Windows.Forms.Button();
+            this.MonoRenderWindow = new MonoGame.Forms.DX.Controls.SampleControl();
+            this.PanelNavbar.SuspendLayout();
             this.SuspendLayout();
             // 
             // PanelItemExplorer
@@ -39,6 +41,7 @@ namespace MonoGame.Forms.DX {
             this.PanelItemExplorer.Name = "PanelItemExplorer";
             this.PanelItemExplorer.Size = new System.Drawing.Size(1184, 115);
             this.PanelItemExplorer.TabIndex = 1;
+            this.PanelItemExplorer.MouseEnter += new System.EventHandler(this.MainWindow_Hover);
             // 
             // PanelFileExplorer
             // 
@@ -49,6 +52,36 @@ namespace MonoGame.Forms.DX {
             this.PanelFileExplorer.Name = "PanelFileExplorer";
             this.PanelFileExplorer.Size = new System.Drawing.Size(200, 617);
             this.PanelFileExplorer.TabIndex = 2;
+            this.PanelFileExplorer.MouseEnter += new System.EventHandler(this.MainWindow_Hover);
+            // 
+            // PanelNavbar
+            // 
+            this.PanelNavbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.PanelNavbar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelNavbar.Controls.Add(this.ButtonFiles);
+            this.PanelNavbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelNavbar.Location = new System.Drawing.Point(0, 0);
+            this.PanelNavbar.Name = "PanelNavbar";
+            this.PanelNavbar.Size = new System.Drawing.Size(1184, 29);
+            this.PanelNavbar.TabIndex = 3;
+            this.PanelNavbar.MouseEnter += new System.EventHandler(this.MainWindow_Hover);
+            // 
+            // ButtonFiles
+            // 
+            this.ButtonFiles.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ButtonFiles.FlatAppearance.BorderSize = 0;
+            this.ButtonFiles.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(89)))), ((int)(((byte)(89)))));
+            this.ButtonFiles.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(13)))), ((int)(((byte)(13)))));
+            this.ButtonFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ButtonFiles.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.ButtonFiles.Location = new System.Drawing.Point(0, 0);
+            this.ButtonFiles.Name = "ButtonFiles";
+            this.ButtonFiles.Size = new System.Drawing.Size(50, 27);
+            this.ButtonFiles.TabIndex = 0;
+            this.ButtonFiles.Text = "File";
+            this.ButtonFiles.UseVisualStyleBackColor = true;
+            this.ButtonFiles.Click += new System.EventHandler(this.ButtonFiles_Click);
             // 
             // MonoRenderWindow
             // 
@@ -61,18 +94,9 @@ namespace MonoGame.Forms.DX {
             this.MonoRenderWindow.Size = new System.Drawing.Size(984, 617);
             this.MonoRenderWindow.TabIndex = 0;
             this.MonoRenderWindow.Text = "Sample Control";
+            this.MonoRenderWindow.MouseEnter += new System.EventHandler(this.MainWindow_Hover);
             // 
-            // PanelNavbar
-            // 
-            this.PanelNavbar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
-            this.PanelNavbar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.PanelNavbar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelNavbar.Location = new System.Drawing.Point(0, 0);
-            this.PanelNavbar.Name = "PanelNavbar";
-            this.PanelNavbar.Size = new System.Drawing.Size(1184, 29);
-            this.PanelNavbar.TabIndex = 3;
-            // 
-            // Form1
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -81,19 +105,21 @@ namespace MonoGame.Forms.DX {
             this.Controls.Add(this.PanelFileExplorer);
             this.Controls.Add(this.PanelItemExplorer);
             this.Controls.Add(this.PanelNavbar);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainWindow";
+            this.Text = "Dreamland Editor";
+            this.PanelNavbar.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
         private Controls.SampleControl MonoRenderWindow;
+
         private System.Windows.Forms.FlowLayoutPanel PanelItemExplorer;
         private System.Windows.Forms.Panel PanelFileExplorer;
         private System.Windows.Forms.Panel PanelNavbar;
+        private System.Windows.Forms.Button ButtonFiles;
     }
 }
 
